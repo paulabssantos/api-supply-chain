4aa14433160fec6314c3ba7a4b980f00ed6c306e
paulabssantos <paulablsantos7@gmail.com>
2023-09-22 09:04:11 -0400

    fix: remove object value e deixa todos os atributos na entity de pedido de compra

--- Src/Domain/Entities/PurchaseOrder.entity.cs
+++ Src/Domain/Entities/PurchaseOrder.entity.cs
@@ -3,14 +3,11 @@ using System.ComponentModel.DataAnnotations;
 public class PurchaseOrder
 {
     [Key]
-    public int Id {get;set;}
+    public int Id { get; set; }
 
-    [Required(ErrorMessage = "Id da cotação é obrigatório")]
-    public int QuotationId { get; set; }
-    public virtual Quotation Quotation { get; set; }
-    
-    [Required(ErrorMessage = "Dados do status do pedido de compras é obrigatório")]
-    public virtual PurchaseOrderStatus PurchaseOrderStatus { get; set; }
+    [Required(ErrorMessage = "Status do pedido de compra é obrigatório")]
+    public int Status { get; set; }
+    public string Justification { get; set; }
     public virtual List<ValueAnalisys> ValueAnalisys { get; set; }
 
 }
--- Src/Domain/ObjectValues/PurchaseOrderStatus.objectValue.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public class PurchaseOrderStatus{
-
-    [Required(ErrorMessage = "Status do pedido de compra é obrigatório")]
-    public int Status {get;set;}
-    public string Justification {get;set;}
-}   
