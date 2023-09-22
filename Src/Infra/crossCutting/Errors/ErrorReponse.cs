661624f31ece42b4e81691bf051253f8238a59ff
paulabssantos <paulablsantos7@gmail.com>
2023-09-22 12:57:03 -0400

    feat: cria classe de resposta de erro

---
+++ Src/Infra/crossCutting/Errors/ErrorReponse.cs
@@ -0,0 +1,10 @@
+public class ErrorResponse
+{
+    public string message { get; }
+    public int statusCode { get; }
+    public ErrorResponse(string _message = "Erro no servidor", int _statusCode = 500)
+    {
+        statusCode = _statusCode;
+        message = _message;
+    }
+}
