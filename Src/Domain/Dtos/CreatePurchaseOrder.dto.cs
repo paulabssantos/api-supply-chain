ca0314d1997cce9b187de57101cd048791c74c7b
paulabssantos <paulablsantos7@gmail.com>
2023-09-22 09:07:51 -0400

    feat:adiciona validacoes e lista de ids de análises de valor

--- Src/Domain/Dtos/CreatePurchaseOrder.dto.cs
+++ Src/Domain/Dtos/CreatePurchaseOrder.dto.cs
@@ -1,8 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 
 public interface CreatePurchaseOrderDto{
-    [Required(ErrorMessage = "Id da análise de valor é obrigatório")]
-    public int ValueAnalysisId {get;set;}
+    [Required(ErrorMessage = "Ids das análises de valor escolhidas são obrigatórios")]
+    [MinLength(1, ErrorMessage = "É preciso de pelo menos um fornecedor escolhido para criar o pedido de compra")]
+    public List<int> ValueAnalysisId {get;set;}
 
     [Required(ErrorMessage = "Status do pedido de compra é obrigatório")]
     [EnumDataType(typeof(PurchaseOrderStatusEnum),ErrorMessage = "Status do pedido de compra precisa ser 1(aprovado),2(reprovado) ou 3 (em análise)")]
