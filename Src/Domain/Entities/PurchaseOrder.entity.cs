64df31d5f36e18fb9e3af4eda8d3ece7f5e94e61
paulabssantos <paulablsantos7@gmail.com>
2023-09-22 08:02:47 -0400

    feat: adiciona id da cotacao como atributo da entidade de pedido de compra

--- Src/Domain/Entities/PurchaseOrder.entity.cs
+++ Src/Domain/Entities/PurchaseOrder.entity.cs
@@ -4,6 +4,11 @@ public class PurchaseOrder
 {
     [Key]
     public int Id {get;set;}
+
+    [Required(ErrorMessage = "Id da cotação é obrigatório")]
+    public int QuotationId { get; set; }
+    public virtual Quotation Quotation { get; set; }
+    
     [Required(ErrorMessage = "Dados do status do pedido de compras é obrigatório")]
     public virtual PurchaseOrderStatus PurchaseOrderStatus { get; set; }
     public virtual List<ValueAnalisys> ValueAnalisys { get; set; }
