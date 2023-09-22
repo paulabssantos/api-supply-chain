6c1a551a440c180cd7d36aaaf68c09909a08e1e8
paulabssantos <paulablsantos7@gmail.com>
2023-09-22 09:08:19 -0400

    feat: adiciona Id e Id da cotacao

--- Src/Domain/Entities/ProductAnalisysValueRequest.entity.cs
+++ Src/Domain/Entities/ProductAnalisysValueRequest.entity.cs
@@ -2,10 +2,18 @@ using System.ComponentModel.DataAnnotations;
 
 public class ProductAnalisysValueRequest
 {
+
+    [Key]
+    public int Id {get;set;}
+
     [Required(ErrorMessage = "Id do produto é obrigatório")]
     public int ProductId { get; set; }
 
     public virtual Product Product { get; set; }
+
+    [Required(ErrorMessage = "Id da cotação é obrigatório")]
+    public int QuotationId {get;set;}
+    public virtual Quotation Quotation {get;set;}
 
     [Required(ErrorMessage = "Quantidade do produto a ser analisado é obrigatória")]
     public int Quantity { get; set; }
