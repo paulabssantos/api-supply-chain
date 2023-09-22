5967da35e16e1ea290906863236cf33d720e5785
paulabssantos <paulablsantos7@gmail.com>
2023-09-22 12:59:02 -0400

    fix: adiciona automapper e servicos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CreateValueAnalisysService>();
+builder.Services.AddScoped<CreatePurchaseOrderService>();
+
+//Libraries
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
