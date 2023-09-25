f4d41efc03fa3c3ff55e94c3d8b06ab2b5846f43
paulabssantos <paulablsantos7@gmail.com>
2023-09-24 20:36:02 -0400

    fix: remove obrigatoriedade de justificativa

--- Migrations/20230924225135_Atualiza justification como opcional.Designer.cs
+++ Migrations/20230924225135_Atualiza justification como opcional.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace supply_chain_back.Migrations
 {
     [DbContext(typeof(MySqlContext))]
-    partial class MySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20230924225135_Atualiza justification como opcional")]
+    partial class Atualizajustificationcomoopcional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230924225135_Atualiza justification como opcional.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace supply_chain_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class Atualizajustificationcomoopcional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Justification",
+                table: "PurchaseOrder",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PurchaseOrder",
+                keyColumn: "Justification",
+                keyValue: null,
+                column: "Justification",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Justification",
+                table: "PurchaseOrder",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
